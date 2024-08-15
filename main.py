616ea4f997a7d274509ae424af5d0c9c6b235b6d
danarhangudya <danarhangudya@gmail.com>
2024-08-15 23:04:14 +0700

    hallo ini adalah project pertama dengan python

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """"
 hallo ini adalah project pertama dengan python
 """
-
-print("hello world!!!")
-print("my name bebek")
+#sequensial
+print('ibu memberi perintah "beli 1 botol susu dan 6 butir telur"')
+print('anak menjawab " Oke "')
+print('anak pergi ke toko')
+print('anak pergi ke toko')
