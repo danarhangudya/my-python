7a9a4667038884161f283cfd8804b3cff3682580
danarhangudya <danarhangudya@gmail.com>
2024-08-15 23:15:29 +0700

    hallo ini adalah project pertama dengan python

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ hallo ini adalah project pertama dengan python
 print('ibu memberi perintah "beli 1 botol susu dan 6 butir telur"')
 print('anak menjawab " Oke "')
 print('anak pergi ke toko')
-print('anak pergi ke toko')
+
+# PERCABANGAN: Eksekusi terpilih
+apakah_susu_ada = True
+if apakah_susu_ada:
+    print('beli susu, "uang cukup"')
+    apakah_telur_ada = False
+    if apakah_telur_ada:
+        print('Anak membeli susu 1 botol dan telur 5 butir')
+    else:
+        print('Anak membeli susu 1 botol')
+else:
+    print('tidak jadi beli susu,"karena susu habis"')
+
+
+
+print('anak pulang ke rumah')
+print('menyerahkan belanjaan ke ibu')
